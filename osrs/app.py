b1ce39faf57d652b1f3918e2e9770527d5ed017a
William Juang <juangw@umich.edu>
2021-01-29 00:41:53 -0500

    setup dynamic port

--- osrs/app.py
+++ osrs/app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
+import os
+
 app = FastAPI()
 
 from osrs.routes.healthcheck import *
 from osrs.routes.highscores import *
 from osrs.routes.grand_exchange import *
+
+PORT = int(os.environ.get("PORT", 8000))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
