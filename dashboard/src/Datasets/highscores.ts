a55dd32ca91e8dbd3fb66138be132566fa6adfb1
William Juang <juangw@umich.edu>
2021-02-01 01:20:32 -0500

    check for env before calling backend

--- dashboard/src/Datasets/highscores.ts
+++ dashboard/src/Datasets/highscores.ts
@@ -1,7 +1,16 @@
 export async function getHistoricalHighscoresForUser(username: string) {
-    const response = await fetch(
-        `http://localhost:8000/highscores/historical/${encodeURIComponent(username)}`
-    );
+    const environment = process.env.ENV
+    let response;
+    if (environment == "local") {
+        response = await fetch(
+            `http://localhost:8000/highscores/historical/${encodeURIComponent(username)}`
+        );
+    } else {
+        response = await fetch(
+            `https://osrs-progress-tracker.herokuapp.com/highscores/historical/${encodeURIComponent(username)}`
+        );
+    }
+    
     const data = await response.json();
     return data;
 }
