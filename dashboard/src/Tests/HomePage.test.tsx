8ec0e81e9c06b3d90e5a6acc336786e4d2720217
William Juang <juangw@umich.edu>
2022-07-08 02:40:19 -0400

    fix unit test

--- dashboard/src/Tests/HomePage.test.tsx
+++ dashboard/src/Tests/HomePage.test.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { HomePage } from "../HomePage";
 
 
 it("Renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<HomePage />, div);
+  const queryClient = new QueryClient()
+  ReactDOM.render(
+    <QueryClientProvider client={queryClient}>
+      <HomePage />
+    </QueryClientProvider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
